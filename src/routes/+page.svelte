13de3fe7bdc00a606078dc95a3597a9c0b02cbeb
Anna Aurora <anna@annaaurora.eu>
2022-12-25 19:51:02 +0100

    There can now be several authenticated lists.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,54 +1,99 @@
 <script>
 	import PocketBase from 'pocketbase';
 	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
 
-	// The app won't work without you setting `POCKETBASE_URL` with dotenv. The default pocketbase URL is `http://127.0.0.1:8090`.
+	// The app won't work without you setting `VITE_POCKETBASE_URL` with dotenv. The default pocketbase URL is `http://127.0.0.1:8090`.
 	const client = new PocketBase(import.meta.env.VITE_POCKETBASE_URL);
 
+	let user;
+
 	$: items = [];
+	$: listRecord = {items: []};
 
 	async function getItems() {
-		items = await client.collection('items').getFullList(1, 20, {
-			sort: '-done',
-		});
+		setTimeout(async function cb() {
+			console.log("Using list…");
+			const record = await client.collection('lists').getOne(user.list, {
+				expand: 'items',
+				key: user.key
+			});
+
+			// Subscribe to not yet subscribed items in list and make record and items globally available.
+			let itemIds = [];
+			for (const item of items) {
+				itemIds.push(item.id);
+			};
+			console.log(record);
+			console.log("items" in record.expand);
+			if ("items" in record.expand) {
+				for (const item of record.expand.items) {
+					if (itemIds.includes(item.id) == false) {
+						client.collection("items").subscribe(item.id, function (e) {
+							getItems();
+						});
+						console.log(`Subscribed to item ${item.id}.`);
+					};
+				};
+
+				items = record.expand.items;
+			};
+			listRecord = record;
+		}, 0);
 	}
 
-	getItems();
-	
 	async function updateItems() {
-		await client.realtime.subscribe('items', function (e) {
+		getItems();
+
+		// Subscribe to list record in case new items are added.
+		client.collection("lists").subscribe(user.list, function (e) {
 			getItems();
 		});
 	}
 
 	// Only runs when client loads site. We don't want the server listening for changes to the databse.
 	onMount(async () => {
+		const userId = $page.url.searchParams.get('user');
+		console.log(`Set user to: ${userId}`);
+		const userPassword = $page.url.searchParams.get('password');
+		console.log(`Set password to: ${userPassword}`);
+
+		const userApiResponse = await client.collection('users').authWithPassword(
+			userId,
+			userPassword
+		);
+		user = userApiResponse.record;
+		console.log(user);
+
 		updateItems();
 	});
 
 	async function toggleItems(toBeToggledItems) {
 		for (let item of toBeToggledItems) {
+			console.log(item.id);
 			if (item.done) {
-				await client.collection('items').update(item.id, {done: false});
+				item.done = false;
+				client.collection('items').update(item.id, {done: false});
 			} else {
-				await client.collection('items').update(item.id, {done: true});
+				item.done = true;
+				client.collection('items').update(item.id, {done: true});
 			}
 		}
 	}
 
 	async function checkItem(item, check) {
 		// Only the database needs to be updated because the item is already set done by `bind:checked={item.done}`.
-		await client.collection('items').update(item.id, { done: item.done, name: item.name });
+		client.collection('items').update(item.id, {done: item.done});
 	}
 
 	async function checkItems(toBeCheckedItems, check) {
 		for (let item of toBeCheckedItems) {
-			if (check) {
+			if (check && item.done == false) {
 				item.done = true;
-				await client.collection('items').update(item.id, {done: true});
-			} else {
+				client.collection('items').update(item.id, {done: true});
+			} else if (check == false && item.done) {
 				item.done = false;
-				await client.collection('items').update(item.id, {done: false});
+				client.collection('items').update(item.id, {done: false});
 			}
 		}
 		// `getItems()` is not needed here because the items were already checked locally.
@@ -56,19 +101,41 @@
 	}
 
 	async function deleteItem(id) {
-		await client.collection('items').delete(id);
+		client.collection('items').delete(id);
 	}
 
 	async function deleteItems(toBeDeletedItems) {
-		for (let item of toBeDeletedItems) {
-			await client.collection('items').delete(item.id);
+		for (const item of toBeDeletedItems) {
+			client.collection('items').delete(item.id);
 		}
 	}
 
 	let answer = '';
 
+	function generatePassword() {
+		// Cryptographically secure password generator copied from <https://stackoverflow.com/a/29770068>. The password is about 50 characters long.
+		return window.crypto.getRandomValues(new BigUint64Array(4)).reduce(
+			(prev, curr, index) => (
+				!index ? prev : prev.toString(36)
+			) + (
+				index % 2 ? curr.toString(36).toUpperCase() : curr.toString(36)
+			)
+		).split('').sort(() => 128 -
+			window.crypto.getRandomValues(new Uint8Array(1))[0]
+		).join('');
+	}
+
 	async function createItem() {
-		await client.collection('items').create({ done: false, name: answer });
+		const createdItemRecord = await client.collection('items').create({ done: false, name: answer, list: user.list });
+
+		console.log(createdItemRecord);
+		console.log(listRecord);
+
+		listRecord.items.push(createdItemRecord.id);
+		const data = {
+			items: listRecord.items
+		};
+		client.collection('lists').update(user.list, data);
 	}
 </script>
 
